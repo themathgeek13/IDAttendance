5ff6fe0e0d5f830bda9e64041c45ba9d2fd718bf
Rohan Rao <rohanrao123@hotmail.com>
2015-08-26 14:39:12 +0530

    Update idattend.py

--- idattend.py
+++ idattend.py
@@ -4,18 +4,6 @@ import getpass
 # Browser
 br = mechanize.Browser()
 
-# Browser options
-br.set_handle_equiv(True)
-br.set_handle_redirect(True)
-br.set_handle_referer(True)
-br.set_handle_robots(False)
-
-# Follows refresh 0 but not hangs on refresh > 0
-br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
-
-# User-Agent Headers
-br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
-
 # Open Login Page and login using browser
 br.open("https://courses.iitm.ac.in/login/index.php")
 br.select_form(nr=0)
@@ -34,7 +22,11 @@ for item in br.links():
         br.open(item.url)
         break
 
-br.select_form(nr=0)
+for newitem in br.links():
+   if 'questions' in str(newitem):
+      br.open(newitem.url)
+      break
+  
 br.form['q807']=['y']
 br.submit()
 print 'Successfully put attendance! :)'
